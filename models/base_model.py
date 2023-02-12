df18c8ae6f15433b12111edebc06c16bc35ac413
Nightburnn <nightburn2003@gmail.com>
2023-02-12 02:18:27 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,65 @@
-#!/usr/bin/env python3
-"""A Base class with common methods/attributes
-for other classes
-"""
-import uuid
-from datetime import datetime, time, date
-from models import storage
+#!/usr/bin/python3
+
+"""Defines the BaseModel"""
+import models
+
+from uuid import uuid4
+
+from datetime import datetime
 
 
 class BaseModel:
-    """a class that defines attributes id,
-    created_at, updated_at and methods
-    """
+    """Represents of BaseModel."""
 
     def __init__(self, *args, **kwargs):
-        """constructor for class attrs id
-        created_at and updated_at
+        """Initialize constructor
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for d, n in kwargs.items():
+                if d == "created_at" or d == "updated_at":
+                    self.__dict__[d] = datetime.strptime(n, tform)
+                else:
+                    self.__dict__[d] = n
         else:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.\
-                                strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """returns a string repr of the class
-        """
-
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+            models.storage.new(self)
 
     def save(self):
-        """updates the updated_at attr
-        """
 
-        self.updated_at = datetime.now()
-        storage.save()
+        """Update updated_at with the current datetime."""
+
+        self.updated_at = datetime.today()
+
+        models.storage.save()
+
+    def __str__(self):
+
+        """__str__ method for BaseModel"""
+
+        class_name = self.__class__.__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def to_dict(self):
-        """returns a dictionary repr of the instance
+
+        """returns a dictionary containing all keys/values
+
+           of __dict__ of the instance
+
         """
-        my_dict = self.__dict__.copy()
-        my_dict.update({
-            "__class__": self.__class__.__name__,
-            "updated_at": self.updated_at.isoformat(),
-            "created_at": self.created_at.isoformat()
-            })
-        return my_dict
+
+        instance_dict = self.__dict__.copy()
+
+        instance_dict["created_at"] = self.created_at.isoformat()
+
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+
+        instance_dict["__class__"] = self.__class__.__name__
+
+        return instance_dict
