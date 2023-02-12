35c80f87023b7d962b3a70fcee9354075b390e57
sethdanny <naddulidaniel1994@gmail.com>
2023-02-12 16:09:29 +0300

    fixed some indentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,36 @@
-#!/usr/bin/python3
-""" module for filestorage class """
+#!/usr/bin/env python3
+"""serializes and deserializes objects to
+and from json
+"""
 
 import json
 
 
-class FileStorage():
-    """ filestorage class to serialize and deserialize json """
+class FileStorage:
+    """defined a class to serialize and
+    deserialize json
+    """
 
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-
-        """ defines our constructor """
-        pass
-
     def all(self):
-
-        """ gets all attributies """
+        """returns returns the dictionary __objects
+        """
 
         return FileStorage.__objects
 
     def new(self, obj):
-
-        """ registers a new object """
-
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-
+        """sets in __objects the obj with key <obj class name>.id
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
-
-        """ saves all objects to a file """
-
+        """serializes __objects to the JSON file (path: __file_path)
+        """
         my_dict = {}
-
         for key, value in FileStorage.__objects.items():
-
             my_dict[key] = value.to_dict()
 
         with open(FileStorage.__file_path, "w") as f:
@@ -44,55 +38,31 @@ class FileStorage():
             json.dump(my_dict, f)
 
     def reload(self):
-
-        """ deserialize from a json file """
-
+        """deserializes from a json file
+        """
         from models.base_model import BaseModel
-
         from models.user import User
-
         from models.state import State
-
         from models.city import City
-
         from models.amenity import Amenity
-
         from models.place import Place
-
         from models.review import Review
 
         class_dict = {
-
-            "BaseModel": BaseModel,
-
-            "User": User,
-
-            "State": State,
-
-            "City": City,
-
-            "Amenity": Amenity,
-
-            "Place": Place,
-
-            "Review": Review
-
-            }
-
+                "BaseModel": BaseModel,
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review
+                }
         obj = {}
-
         try:
-
             with open(FileStorage.__file_path, "r") as f:
-
                 json_data = json.load(f)
-
                 for key, value in json_data.items():
-
                     obj[key] = class_dict[value["__class__"]](**value)
-
                 FileStorage.__objects = obj
-
         except FileNotFoundError:
-
             pass
