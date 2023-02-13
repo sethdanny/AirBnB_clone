fcaa9cb4de2f3393f2128ec9018f3aa3468b1cb6
sethdanny <naddulidaniel1994@gmail.com>
2023-02-13 23:37:10 +0300

    fixed place model

--- models/place.py
+++ models/place.py
@@ -14,7 +14,8 @@ class Place(BaseModel):
     name = ""
     description = ""
     number_rooms = 0
-    max_guests = 0
+    number_bathrooms = 0
+    max_guest = 0
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
